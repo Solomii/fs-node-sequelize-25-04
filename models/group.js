d05ee837c24c8367715f020f22a44f6e73437488
Solomiia Kushniryk <kushniryksolomiya@gmail.com>
2023-05-01 14:55:05 +0300

    Add associate to models group and user

--- models/group.js
+++ models/group.js
@@ -10,7 +10,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Group.belongsToMany(models.User, {
+        through:'users_to_groups',
+        foreignKey:'groupId'
+      })
     }
   }
   Group.init({
